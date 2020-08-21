596ab76547bb83407d265d142d42c8883cfb152c
Crowdbotics <team@crowdbotics.com>
2020-08-21 19:03:13 +0000

    Initial commit.

---
+++ testfupdates_dev_9259/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for testfupdates_dev_9259 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testfupdates_dev_9259.settings')
+
+application = get_wsgi_application()
